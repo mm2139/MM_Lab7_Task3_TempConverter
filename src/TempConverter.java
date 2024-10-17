45423de6d88fed8e827e764682ff9971151cefc5
mmaalin <maalin.622139@swl.k12.oh.us>
2024-10-17 08:22:24 -0400

    fin

---
+++ src/TempConverter.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class TempConverter {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double temp = 0;
+        String cont = "Y";
+        Boolean contHasResponse = false;
+
+        do {
+            if (cont.equalsIgnoreCase("Y")) {
+                System.out.println("Please input a temperature to change from Farenheit to Celsius.");
+                if (scan.hasNextDouble()) {
+                    temp = scan.nextDouble();
+                    scan.nextLine();
+                    System.out.println("The temperature " + temp + " converted to Farenheit is: " + ((temp - 32) * 5 / 9) + ".");
+                    System.out.println("Would you like to play again?");
+                    do {
+                        if (scan.hasNextLine()) {
+                            cont = scan.nextLine();
+                            if (cont.equalsIgnoreCase("Y") || cont.equalsIgnoreCase("N")) {
+                                contHasResponse = true;
+                            } else {
+                                scan.nextLine();
+                                System.out.println("You have inputted an invalid response - please try again.");
+                            }
+                        } else {
+                            scan.nextLine();
+                            System.out.println("You have inputted an invalid response - please try again.");
+                        }
+                    } while (contHasResponse == false);
+                } else {
+                    System.out.println("You have inputted an invalid response - please try again.");
+                    scan.nextLine();
+                }
+            }
+        } while (!cont.equalsIgnoreCase("N"));
+
+        System.out.println("Thank you for playing!");
+        System.exit(0);
+
+    }
+}
